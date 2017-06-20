c88e3d3deb84e7060ad2005648b77139ccff7292
culling <culling66@gmail.com>
2017-06-21 06:55:22 +0800

    fixed the ISBN to come from the raw object

--- app/controllers/books.controller.server.js
+++ app/controllers/books.controller.server.js
@@ -30,29 +30,27 @@ exports.lookup = function(title, callback){
 
 exports.create  = function(rawBookObject){
     console.log("Book - Create Called");
-    //console.log("bookOwner")
-    //console.log(rawBookObject.owner );
 
-
-
+    //Find the ISBN Numbers
     var isbnObjects = rawBookObject.volumeInfo.industryIdentifiers;
-        
+    var ISBN_13 = isbnObjects.filter((isbnObject)=> { return isbnObject.type === "ISBN_13"} )[0];
+    var ISBN_10 = isbnObjects.filter((isbnObject)=> { return isbnObject.type === "ISBN_10"} )[0];
     
-    var book = {
-        authors: rawBookObject.volumeInfo.authors ,
-        title: rawBookObject.volumeInfo.title ,
-        owner: rawBookObject.owner._id,
-        thumbnailUrl: rawBookObject.volumeInfo.imageLinks.smallThumbnail ,
-        ISBN_13: "1" ,
-        ISBN_10: "1"
-    }
-    
-    console.log();
-    var newBook = new BookModel(book);
-    console.log(newBook);
 
-    
-    newBook.save();
+
+    if(rawBookObject.owner){
+        var book = {
+            authors: rawBookObject.volumeInfo.authors ,
+            title: rawBookObject.volumeInfo.title ,
+            owner: rawBookObject.owner._id,
+            thumbnailUrl: rawBookObject.volumeInfo.imageLinks.smallThumbnail ,
+            ISBN_13: ISBN_13.identifier ,
+            ISBN_10: ISBN_10.identifier
+        }
+        var newBook = new BookModel(book);
+        console.log(newBook);
+        newBook.save();
+    }
 };
 
 
